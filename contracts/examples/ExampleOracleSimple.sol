a571ed4f1c532c8fdc6024923f2374649981a9f4
hank <hank@show.one>
2020-11-09 18:02:15 +0800

    debug

--- contracts/examples/ExampleOracleSimple.sol
+++ contracts/examples/ExampleOracleSimple.sol
@@ -1,11 +1,11 @@
 pragma solidity =0.6.6;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import '@beefswap/core/contracts/interfaces/IBeefswapV2Factory.sol';
+import '@beefswap/core/contracts/interfaces/IBeefswapV2Pair.sol';
 import '@uniswap/lib/contracts/libraries/FixedPoint.sol';
 
-import '../libraries/UniswapV2OracleLibrary.sol';
-import '../libraries/UniswapV2Library.sol';
+import '../libraries/BeefswapV2OracleLibrary.sol';
+import '../libraries/BeefswapV2Library.sol';
 
 // fixed window oracle that recomputes the average price for the entire period once every period
 // note that the price average is only guaranteed to be over at least 1 period, but may be over a longer period
@@ -14,7 +14,7 @@ contract ExampleOracleSimple {
 
     uint public constant PERIOD = 24 hours;
 
-    IUniswapV2Pair immutable pair;
+    IBeefswapV2Pair immutable pair;
     address public immutable token0;
     address public immutable token1;
 
@@ -25,7 +25,7 @@ contract ExampleOracleSimple {
     FixedPoint.uq112x112 public price1Average;
 
     constructor(address factory, address tokenA, address tokenB) public {
-        IUniswapV2Pair _pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
+        IBeefswapV2Pair _pair = IBeefswapV2Pair(BeefswapV2Library.pairFor(factory, tokenA, tokenB));
         pair = _pair;
         token0 = _pair.token0();
         token1 = _pair.token1();
@@ -39,14 +39,14 @@ contract ExampleOracleSimple {
 
     function update() external {
         (uint price0Cumulative, uint price1Cumulative, uint32 blockTimestamp) =
-            UniswapV2OracleLibrary.currentCumulativePrices(address(pair));
+            BeefswapV2OracleLibrary.currentCumulativePrices(address(pair));
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
 
         // ensure that at least one full period has passed since the last update
         require(timeElapsed >= PERIOD, 'ExampleOracleSimple: PERIOD_NOT_ELAPSED');
 
         // overflow is desired, casting never truncates
-        // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
+        // cumulative price is in (uq112x112 price * seconds) beefts so we simply wrap it after division by time elapsed
         price0Average = FixedPoint.uq112x112(uint224((price0Cumulative - price0CumulativeLast) / timeElapsed));
         price1Average = FixedPoint.uq112x112(uint224((price1Cumulative - price1CumulativeLast) / timeElapsed));
 
--- contracts/examples/README.md
+++ contracts/examples/README.md
@@ -3,12 +3,12 @@
 ## Disclaimer
 
 These contracts are for demonstrative purposes only.
-While these contracts have unit tests, and we generally expect them to be
+While these contracts have beeft tests, and we generally expect them to be
 correct, there are no guarantees about the correctness or security of 
 these contracts. We hold these contracts to a different standard of 
 correctness and security than other contracts in this repository. 
 E.g., we have explicitly excluded these contracts from the
-[bug bounty](https://uniswap.org/bug-bounty/#scope). 
+[bug bounty](https://beefswap.org/bug-bounty/#scope). 
 
 You must do your own due diligence if you wish to use code
 from these examples in your project.
